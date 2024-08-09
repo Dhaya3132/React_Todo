55ebac7ce70e4bcda97907d3bb330fa2b8197833
Dhaya3132 <dhayalanu103@gmail.com>
2024-08-09 23:39:35 +0530

    Feat - Editing a text

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -24,7 +24,7 @@ const TodoList = ({ data, onDelete, onComplete,onSave }) => {
       <ul className="bg-gray-50 p-5 rounded-xl flex flex-col gap-5 shadow-xl">
         {data.map((item) => (
           <li className="border-solid border-2 border-slate-200 rounded-md p-2.5" key={item.id}>
-            {editId === item.id ? (<div><input type="text" value={editText} onChange={(e) => setEditText(e.target.value)} /> <button onClick={()=>handleSave(item.id)}>Save</button></div>):(<h2 className="font-Sora md:text-lg font-light text-600 text-base">Todo: <span className="font-Sora font-semibold md:text-3xl text-xl">{item.text}</span></h2>)}
+            {editId === item.id ? (<div><input type="text" value={editText} onChange={(e) => setEditText(e.target.value)} className='border-2 border-solid border-slate-200 bg-transparent rounded-xl p-2' /> <button onClick={()=>handleSave(item.id)} className='bg-blue-400 px-5 py-2 text-Sora font-semibold text-white rounded-xl'>Save</button></div>):(<h2 className="font-Sora md:text-lg font-light text-600 text-base">Todo: <span className="font-Sora font-semibold md:text-3xl text-xl">{item.text}</span></h2>)}
             <div className="flex items-center justify-end mt-3 gap-3">
               <button onClick={() => handleEdit(item.id, item.text)} className='font-Sora text-xs font-semibold items-center border-solid border-2 border-slate-200 px-3 py-3 rounded-2xl hover:bg-blue-600 hover:text-white'>
                 Edit
